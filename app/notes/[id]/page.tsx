3ebf10c3a0c43e98f4f41d660e4dba6a5810553d
unknown <ab261099tro@H-ab261099tro.ws.a-bank.com.ua>
2025-06-27 09:12:34 +0300

    change

---
+++ app/notes/[id]/page.tsx
@@ -0,0 +1,26 @@
+import { QueryClient, HydrationBoundary, dehydrate } from '@tanstack/react-query';
+import { fetchNoteById } from "../../lib/api"
+import NoteDetailsClient from './NoteDetails.client';
+
+interface Props {
+  params: Promise<{ id: string }>;
+}
+
+const NoteDetails = async ({ params }: Props) => {
+  const { id } = await params;
+  const numericId = Number(id);
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['note', numericId],
+    queryFn: () => fetchNoteById(numericId),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient />
+    </HydrationBoundary>
+  );
+};
+
+export default NoteDetails;
---
+++ app/notes/page.tsx
@@ -0,0 +1,72 @@
+"use client"
+
+import { useState, useEffect } from 'react';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import NoteList from '../components/NoteList/NoteList';
+import Pagination from '../components/Pagination/Pagination';
+import SearchBox from '../components/SearchBox/SearchBox';
+import NoteModal from '../components/NoteModal/NoteModal';
+import styles from './App.module.css';
+import { useDebounce } from 'use-debounce';
+import { fetchNotes } from '../lib/api';
+import type { Note } from '../types/note';
+
+const App = () => {
+  const [page, setPage] = useState<number>(1);
+  const [search, setSearch] = useState<string>('');
+  const [debouncedSearch] = useDebounce(search, 500);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    setPage(1);
+  }, [search]);
+
+  const { data, isLoading, isError, isSuccess } = useQuery<{
+    notes: Note[];
+    totalPages: number;
+  }>({
+    queryKey: ['notes', debouncedSearch, page],
+    queryFn: () => fetchNotes({ search: debouncedSearch, page }),
+    enabled: debouncedSearch.length >= 0,
+    placeholderData: keepPreviousData,
+  });
+
+  const handleSearchChange = (value: string) => {
+    setSearch(value);
+  };
+
+  const handlePageChange = (newPage: number) => {
+    setPage(newPage);
+  };
+
+  return (
+    <div className={styles.app}>
+      <header className={styles.toolbar}>
+        <SearchBox value={search} onChange={handleSearchChange} />
+        {data?.totalPages && data?.totalPages > 1 && (
+          <Pagination
+            page={page}
+            pageCount={data.totalPages}
+            onChange={handlePageChange}
+          />
+        )}
+        <button className={styles.button} onClick={() => setIsModalOpen(true)}>
+          Create note +
+        </button>
+      </header>
+
+      {data?.totalPages && data?.notes.length > 0 && (
+        <NoteList
+          notes={data.notes}
+          isLoading={isLoading}
+          isError={isError}
+          isSuccess={isSuccess}
+        />
+      )}
+
+      {isModalOpen && <NoteModal onClose={() => setIsModalOpen(false)} />}
+    </div>
+  );
+};
+
+export default App;
